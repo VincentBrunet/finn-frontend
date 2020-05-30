87c2c36782b0e69cd71e5dfa870ce427439d17f3
Vincent Brunet <wincerouge@gmail.com>
2020-05-31 00:32:07 +0200

    up

---
+++ src/services/utils/Api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export class Api {
+  static root = "http://192.168.1.54:3000";
+
+  static async getTickerSummary(code: string) {
+    return await Api.get("/ticker/summary/" + code);
+  }
+
+  static async getScreenerTable() {
+    return await Api.get("/screener/table");
+  }
+
+  private static async get(path: string) {
+    return await (await axios.get(Api.root + path)).data.data;
+  }
+}
