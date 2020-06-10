abac94e8adbe8a11eb25f5c82ee72ee7da54cc8c
Vincent Brunet <wincerouge@gmail.com>
2020-06-10 03:05:02 +0200

    up

--- src/components/interface/Card.tsx
+++ src/components/interface/Card.tsx
@@ -13,9 +13,7 @@ export class Card extends Component {
   onRender() {
     return (
       <FlexItem width="100%">
-        <Box background="#686877" shadow="0 0 8px 0 rgba(0, 0, 0, 0.5)">
-          {this.props.children}
-        </Box>
+        <Box>{this.props.children}</Box>
       </FlexItem>
     );
   }
